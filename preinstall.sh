55c5412ffcd012f33fc9df986617eb0392fd756a
Matthew Jarvis <matt@sixteen.local>
2020-02-23 14:47:19 -0500

    Working with Elastic Stack 7.6, will fix Splunk in the future.

--- preinstall.sh
+++ preinstall.sh
@@ -1,7 +1,7 @@
 ################################################################################
 # Choose Versions (everything else will be 'latest' builds)
 ################################################################################
-ELASTIC_VERSION=7.5.2
+ELASTIC_VERSION=7.6.0
 SPLUNK_VERSION=7.3.0
 CORTEX_VERSION=2.1.3
 THEHIVE_VERSION=3.2.1
